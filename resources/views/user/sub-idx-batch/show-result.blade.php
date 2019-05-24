da79477ec8061eb2bf302bf483678689ed41a1a5
Sjors Ottjes <sjorsottjes@gmail.com>
2019-05-24 13:04:17 +0200

    show lavel

---
+++ resources/views/user/sub-idx-batch/show-result.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.user.template', [
+    'title' => 'Sub/Idx Batch'
+])
+
+@section('content')
+
+    <h1 class="mb-4">Batch {{ $subIdxBatch->label }}</h1>
+
+    <div id="sub-idx-batch-result" data-batch-id="{{ $subIdxBatch->id }}"></div>
+
+@endsection
