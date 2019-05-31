a7de5e1cfdb9e2cc460c8c692d308bd5cfd93eb1
Sjors Ottjes <sjorsottjes@gmail.com>
2019-05-31 13:21:01 +0200

    style

--- resources/views/user/sub-idx-batch/show-linked.blade.php
+++ resources/views/user/sub-idx-batch/show-linked.blade.php
@@ -17,13 +17,22 @@
             You can select the languages you want to extract from the files below on the "start batch" page.
         </div>
 
+        <div class="border-b my-4"></div>
+
         @foreach($subIdxBatch->files as $batchFile)
-        <div class="my-4">
-            <div>Sub: {{ $batchFile->sub_original_name }}</div>
-            <div class="text-xs">Idx: {{ $batchFile->idx_original_name }}</div>
+        <div class="flex p-1 mt-4 hover:bg-grey-lighter">
+            <div class="flex-grow">
+                <div class="mb-2">{{ $batchFile->sub_original_name }}</div>
+                <div class="text-sm">{{ $batchFile->idx_original_name }}</div>
+            </div>
+
             <form action="{{ route('user.subIdxBatch.unlink', $batchFile) }}" method="post">
                 {{ csrf_field() }}
-                <button>unlink</button>
+                <button class="mt-2 mr-2" title="Unlink">
+                    <svg class="h-5 w-5">
+                        <use xlink:href="#unlink"></use>
+                    </svg>
+                </button>
             </form>
         </div>
         @endforeach
